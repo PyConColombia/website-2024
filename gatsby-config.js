225a912f356d8fd79c9bc71f587201062e97912b
Alejandro E. Rendon <alejandro@lendingfront.com>
2023-12-17 08:14:43 -0500

    Pausar configuracion del formato

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,29 +46,29 @@ module.exports = {
       },
       __key: 'pages'
     },
-    {
-      resolve: 'gatsby-plugin-prettier-eslint',
-      options: {
-        prettier: {
-          patterns: [
-            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
-            '**/*.{css,scss,less}',
-            '**/*.{json,json5}',
-            '**/*.{graphql}',
-            '**/*.{md,mdx}',
-            '**/*.{html}',
-            '**/*.{yaml,yml}'
-          ]
-        },
-        eslint: {
-          patterns: '**/*.{js,jsx,ts,tsx}',
-          customOptions: {
-            fix: true,
-            cache: true
-          }
-        }
-      }
-    },
+    // {
+    //   resolve: 'gatsby-plugin-prettier-eslint',
+    //   options: {
+    //     prettier: {
+    //       patterns: [
+    //         // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+    //         '**/*.{css,scss,less}',
+    //         '**/*.{json,json5}',
+    //         '**/*.{graphql}',
+    //         '**/*.{md,mdx}',
+    //         '**/*.{html}',
+    //         '**/*.{yaml,yml}'
+    //       ]
+    //     },
+    //     eslint: {
+    //       patterns: '**/*.{js,jsx,ts,tsx}',
+    //       customOptions: {
+    //         fix: true,
+    //         cache: true
+    //       }
+    //     }
+    //   }
+    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
