8b33a1958270552b9648f528b5a69faae8f6ce52
Alejandro E. Rendon <alejandro@lendingfront.com>
2023-12-24 11:16:01 -0500

    Arreglar llamado a PI de mailchimp

--- src/components/LandingPage/Contact.js
+++ src/components/LandingPage/Contact.js
@@ -6,8 +6,6 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { StaticImage } from 'gatsby-plugin-image';
 
-import axios from 'axios';
-
 const Contact = () => {
   const [email, setEmail] = useState('');
   const [firstName, setFirstName] = useState('');
@@ -17,9 +15,8 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const config = {
-        url:
-          `https://pycon.us21.list-manage.com/subscribe/post-json?` +
+      fetch(
+        `https://pycon.us21.list-manage.com/subscribe/post-json?` +
           new URLSearchParams({
             u: 'b17171278920cd24d0c9c4cfe',
             id: '785e2a687f',
@@ -28,14 +25,18 @@ const Contact = () => {
             EMAIL: email,
             subscribe: 'Subscribe'
           }),
-        method: 'GET'
-      };
+        {
+          method: 'GET',
+          mode: 'no-cors'
+        }
+      ).then((response) => {
+        console.log(response.status);
+        console.log(response);
+      });
 
-      const response = await axios(config);
-
-      console.log("Success!",  response);
+      console.log('Success!', response);
     } catch (error) {
-      console.error("Error:", error);
+      console.error('Error:', error);
     }
   };
 
@@ -44,42 +45,45 @@ const Contact = () => {
       <div id="form-wrapper">
         <Container fluid>
           <Row>
-            <Col xs={12} md={3}>
+            <Col xs={12} md={3} className="d-sm-none d-md-block">
               <StaticImage src="../../static/images/contact/1.svg" alt="Contact Icons" />
             </Col>
             <Col xs={12} md={6} className="justify-content-center">
-              <h2 className="title">Want to know more?</h2>
+              <div id="form-container">
+                <h2 className="title">Want to know more?</h2>
 
-              <Form>
-                <Form.Group className="mb-3">
-                  <Form.Control
-                    type="text"
-                    placeholder="First Name"
-                    onChange={(e) => setFirstName(e.target.value)}
-                  />
-                </Form.Group>
+                <Form>
+                  <Form.Group className="mb-3">
+                    <Form.Control
+                      type="text"
+                      placeholder="First Name"
+                      onChange={(e) => setFirstName(e.target.value)}
+                    />
+                  </Form.Group>
 
-                <Form.Group className="mb-3">
-                  <Form.Control
-                    type="text"
-                    placeholder="Last Name"
-                    onChange={(e) => setLastName(e.target.value)}
-                  />
-                </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Control
+                      type="text"
+                      placeholder="Last Name"
+                      onChange={(e) => setLastName(e.target.value)}
+                    />
+                  </Form.Group>
 
-                <Form.Group className="mb-3">
-                  <Form.Control
-                    type="email"
-                    placeholder="Email"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </Form.Group>
-                <Button variant="primary" onClick={onSumitForm}>
-                  Submit
-                </Button>
-              </Form>
+                  <Form.Group className="mb-3">
+                    <Form.Control
+                      type="email"
+                      placeholder="Email"
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                  <Button variant="primary" onClick={onSumitForm} size="lg" id="button-submit">
+                    Submit
+                  </Button>
+                </Form>
+              </div>
             </Col>
-            <Col xs={12} md={3}>
+            <Col xs={12} md={3} className="d-sm-none d-md-block">
               <StaticImage src="../../static/images/contact/2.svg" alt="Contact Icons" />
             </Col>
           </Row>
