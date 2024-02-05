a362c4955dc3e664305442b139d35f068f619974
Alejandro E. Rendon <alejandro@lendingfront.com>
2024-02-05 18:59:39 -0500

    #25 Modificar banner de la vista

--- src/pages/call-for-proposals/components/Technical.js
+++ src/pages/call-for-proposals/components/Technical.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Technical = () => {
   return (
@@ -13,7 +14,32 @@ const Technical = () => {
             expertise on any of the following subjects:
           </Col>
           <Col xs={12}>
-
+            <div className='list'>
+              <span className='element'>DATA SCIENCE</span>
+              <StaticImage
+                src="../../../static/images/icons/icon-1.svg"
+                alt="data science"
+                className="icon"
+              />
+              <span className='element'>MACHINE LEARNING</span>
+              <StaticImage
+                src="../../../static/images/icons/icon-2.svg"
+                alt="machine learning"
+                className="icon"
+              />
+              <span className='element'>DEEP LEARNING</span>
+              <StaticImage
+                src="../../../static/images/icons/icon-3.svg"
+                alt="deep learning"
+                className="icon"
+              />
+              <span className='element'>ARTIFICIAL INTELLIGENCE</span>
+              <StaticImage
+                src="../../../static/images/icons/icon-4.svg"
+                alt="artificial intelligence"
+                className="icon"
+              />
+            </div>
           </Col>
         </Row>
       </Container>
--- src/pages/call-for-proposals/components/Title.js
+++ src/pages/call-for-proposals/components/Title.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
+
 import Topics from './Topics';
 import Technical from './Technical';
 
@@ -12,7 +15,7 @@ const Title = () => {
         <div className="welcome-bg">
           <Container>
             <Row className="justify-content-center">
-              <Col xs={10} md={7}>
+              <Col xs={10} md={6}>
                 <div className="title-container">
                   <h2 className="title">
                     <span className="bold">Call for proposals</span>
@@ -21,7 +24,7 @@ const Title = () => {
                     Calling on all Python enthusiasts! Submit your proposals for PyCon Colombia 2024
                     now. Don't miss the opportunity to share your insights
                   </p>
-                  <span>Closes at March 11th 2024</span>
+                  <span className="closes-date"><FontAwesomeIcon icon={faCalendar} /> Closes at March 11th 2024</span>
                 </div>
               </Col>
             </Row>
