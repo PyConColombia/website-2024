2e8a4d2d1355338b514799b4cf89df47f8ad2eab
Alejandro E. Rendon <alejandro@rendon.co>
2024-02-25 17:13:51 -0500

    #13 Configurar color para ponentes

--- src/app/[lang]/keynotes/components/Card.js
+++ src/app/[lang]/keynotes/components/Card.js
@@ -4,32 +4,42 @@ import Ratio from 'react-bootstrap/Ratio';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'next/image';
-import Link from 'next/link';
-import speakerslist from '@/data/speakers.json';
-
-import Avatar from '@/app/[lang]/speakers/images/avatar.jpeg';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import { faXTwitter, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
-const Card = ({ speakerData, reverse, lang }) => {
+const Card = ({ speakerData, reverse, index }) => {
+  const colorBerderSpeaker = ['border-pink', 'border-yellow', 'border-purple', 'border-blue'];
+  const colorBerderTextSpeaker = [
+    'text-border-pink',
+    'text-border-yellow',
+    'text-border-purple',
+    'text-border-blue'
+  ];
+
   return (
     <Row className="keynote-card">
       <Col xs={12} md={{ span: 4, order: reverse ? 'last' : 'first' }}>
         <Ratio aspectRatio="1x1">
-          <Image className="img-keynote" src="" alt="Keynote Image" width={300} height={300} />
+          <Image
+            className={`img-keynote ${colorBerderSpeaker[(index + 1) % colorBerderSpeaker.length]}`}
+            src="/images/keynotes/profile.png"
+            alt="Keynote Image"
+            width={300}
+            height={300}
+          />
         </Ratio>
       </Col>
       <Col xs={12} md={8}>
         <Row>
           <Col xs={12} md={{ span: 6, order: reverse ? 'last' : 'first' }}>
-            <h2>
+            <h3>
               <span className="bold">
                 {speakerData.first_name} {speakerData.last_name}
               </span>
-              <span className="flag"> {lang} 🇧🇪 </span>
-            </h2>
+              <span className="flag"> 🇧🇪 </span>
+            </h3>
           </Col>
           <Col xs={12} md={6}>
             <a href="https://twitter.com" target="_blank" rel="noreferrer" className="">
@@ -54,7 +64,8 @@ const Card = ({ speakerData, reverse, lang }) => {
         </Row>
         <Row>
           <Col xs={12} md={{ span: 10 }}>
-            <span className="text-border">
+            <span
+              className={`text-border ${colorBerderTextSpeaker[(index + 1) % colorBerderTextSpeaker.length]}`}>
               <FontAwesomeIcon icon={faCalendarAlt} /> The new age in fintech | 4 Junio 2024
             </span>
           </Col>
@@ -68,7 +79,13 @@ const Card = ({ speakerData, reverse, lang }) => {
 };
 
 Card.propTypes = {
-  reverse: propTypes.bool
+  speakerData: propTypes.shape({
+    first_name: propTypes.string,
+    last_name: propTypes.string,
+    link: propTypes.string
+  }),
+  reverse: propTypes.bool,
+  index: propTypes.number
 };
 
 export default Card;
--- src/app/[lang]/keynotes/page.js
+++ src/app/[lang]/keynotes/page.js
@@ -3,48 +3,16 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import Container from 'react-bootstrap/Container';
-import Ratio from 'react-bootstrap/Ratio';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'next/image';
-import Link from 'next/link';
 import speakerslist from '@/data/speakers.json';
 
-import Avatar from '@/app/[lang]/speakers/images/avatar.jpeg';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle } from '@fortawesome/free-solid-svg-icons';
-import { faXTwitter, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import Card from './components/Card';
 import Title from './components/Title';
 
 const Speakers = ({ params: { lang } }) => {
   return (
     <section id="keynotes">
-      {/* {speakerslist.map(
-        (speaker) =>
-          speaker.type === 'keynote' && (
-            <Row key={speaker.id}>
-              <Col md={4}>
-                <Link href={`/keynotes/${speaker.id}`} locale={lang}>
-                  <Image src={Avatar} width={200} height={200} alt="Picture of the author" />
-                </Link>
-              </Col>
-              <Col md={8}>
-                <Link href={`/keynotes/${speaker.id}`} locale={lang}>
-                  <h1>
-                    {speaker.first_name} {speaker.last_name}
-                  </h1>
-                  <h2>{speaker.affiliation}</h2>
-                </Link>
-                <Link href={`https://twitter.com/${speaker.twitter}`} target="_blank">
-                  <span>{speaker.twitter}</span>
-                </Link>
-              </Col>
-            </Row>
-          )
-      )} */}
-
       <Title />
       <div className="keynotes-bg">
         <Container>
@@ -53,7 +21,7 @@ const Speakers = ({ params: { lang } }) => {
               (speaker, index) =>
                 speaker.type === 'keynote' && (
                   <Col xs={12} md={10} key={speaker.id}>
-                    <Card speakerData={speaker} reverse={index % 2 === 0} lang={lang} />
+                    <Card speakerData={speaker} reverse={index % 2 === 0} index={index} />
                   </Col>
                 )
             )}
