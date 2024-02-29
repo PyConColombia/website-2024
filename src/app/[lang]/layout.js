9b58d767c685083545f160789e3498a25a8d5daa
Alejandro E. Rendon <alejandro@lendingfront.com>
2024-02-29 06:34:00 -0500

    #53 Agregar configuracion para Hotjar

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -5,6 +5,7 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 import { Poppins } from 'next/font/google';
 
 import I18nLayout from '@/app/[lang]/i18n-layout';
+import TrackingLayout from '@/app/[lang]/tracking-layout';
 import NavbarCustom from '@/app/[lang]/components/NavbarCustom';
 
 import '@/styles/styles.sass';
@@ -27,40 +28,42 @@ export const metadata = {
 
 const Root = ({ children, params: { lang } }) => {
   return (
-    <I18nLayout params={{ lang }}>
-      <html lang={lang}>
-        <Head>
-          <title>PyCon Colombia 2024</title>
+    <TrackingLayout>
+      <I18nLayout params={{ lang }}>
+        <html lang={lang}>
+          <Head>
+            <title>PyCon Colombia 2024</title>
 
-          <meta name="description" content="Python conference in Colombia." key="desc"></meta>
-          <meta charset="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-          <meta property="og:type" content="website" />
+            <meta name="description" content="Python conference in Colombia." key="desc"></meta>
+            <meta charset="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+            <meta property="og:type" content="website" />
 
-          <meta property="og:url" content="/" />
-          <meta property="og:site_name" content="PyCon Colombia 2024" />
-          <meta property="og:title" content="PyCon Colombia 2024" />
-          <meta property="og:description" content="Python conference in Colombia." />
-          <meta property="og:image" content="/images/opimage.jpeg" />
-          <meta property="og:image:width" content="500" />
-          <meta property="og:image:height" content="500" />
+            <meta property="og:url" content="/" />
+            <meta property="og:site_name" content="PyCon Colombia 2024" />
+            <meta property="og:title" content="PyCon Colombia 2024" />
+            <meta property="og:description" content="Python conference in Colombia." />
+            <meta property="og:image" content="/images/opimage.jpeg" />
+            <meta property="og:image:width" content="500" />
+            <meta property="og:image:height" content="500" />
 
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@pyconcolombia" />
-          <meta name="twitter:description" content="Python conference in Colombia." />
-          <meta name="twitter:title" content="PyCon Colombia 2024" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@pyconcolombia" />
+            <meta name="twitter:description" content="Python conference in Colombia." />
+            <meta name="twitter:title" content="PyCon Colombia 2024" />
 
-          <link rel="icon" href="/images/favicon.ico" sizes="any" />
-        </Head>
-        <body className={poppins.className}>
-          <AlertNews />
-          <NavbarCustom lang={lang} />
-          <main>{children}</main>
-          <FooterLayout />
-        </body>
-        <GoogleAnalytics gaId="G-RYYD6XKC7Y" />
-      </html>
-    </I18nLayout>
+            <link rel="icon" href="/images/favicon.ico" sizes="any" />
+          </Head>
+          <body className={poppins.className}>
+            <AlertNews />
+            <NavbarCustom lang={lang} />
+            <main>{children}</main>
+            <FooterLayout />
+          </body>
+          <GoogleAnalytics gaId="G-RYYD6XKC7Y" />
+        </html>
+      </I18nLayout>
+    </TrackingLayout>
   );
 };
 
