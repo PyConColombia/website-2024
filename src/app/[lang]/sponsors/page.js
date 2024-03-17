43c3f9d89d39f4a1f6d1c72987f90920c906a0cd
Alejandro E. Rendon <alejandro@rendon.co>
2024-03-17 12:16:19 -0500

    #15 modificar titulo de la pstana

--- src/app/[lang]/sponsors/page.js
+++ src/app/[lang]/sponsors/page.js
@@ -3,6 +3,19 @@ import propTypes from 'prop-types';
 
 import Sponsors from './components/Sponsors';
 
+import en from '@/data/dictionaries/en.json';
+import es from '@/data/dictionaries/es.json';
+
+export async function generateMetadata({ params: { lang } }, parent) {
+  const dataLang = lang === 'en' ? en : es;
+  const dataSection = dataLang?.sections;
+  const sponsorsData = dataSection.sponsors;
+
+  return {
+    title: sponsorsData.title
+  };
+}
+
 const Page = ({ params: { lang } }) => {
   return <Sponsors lang={lang} />;
 };
