98b5931261ff6ab4b57fa0fcf570bdf9c6d8847a
Alejandro E. Rendon <alejandro@rendon.co>
2024-03-23 06:23:49 -0500

    #15 Agregar patrocinadores dependiendo del nivel

--- src/app/[lang]/sponsors/components/Sponsor.js
+++ src/app/[lang]/sponsors/components/Sponsor.js
@@ -1,20 +1,35 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const Sponsor = ({ level, sponsorData }) => {
+  const sponsorSize = {
+    diamond: '12',
+    platinum: '10',
+    gold: '8',
+    silver: '6',
+    bronze: '4'
+  };
+
   return (
-    <div className="card-sponsor">
-      <Row>
-        <Col xs={12} md={3}>
-          <span className={`badge ${level}`}>{level}</span>
-        </Col>
-        <Col xs={12} md={9}>
-          <hr className="separator" />
-        </Col>
-      </Row>
-    </div>
+    <Row>
+      <Col xs={12} md={sponsorSize[level]}>
+        <div className="sponsor-container">
+          <Link href={sponsorData.url || ''} target="_blank">
+            <Image
+              className="img-sponsor"
+              src={`/images/sponsor/${sponsorData.image}`}
+              alt={sponsorData.name}
+              width={300}
+              height={300}
+            />
+          </Link>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
